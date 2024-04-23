726c648d19349455d057ab363811d884baff3308
Piwko Patryk <patryk.piwko@god.dev>
2024-04-23 14:26:42 +0200

    first commit

---
+++ src/main/java/org/example/dockertraining/DockerTrainingApplication.java
@@ -0,0 +1,13 @@
+package org.example.dockertraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/dockertraining/DockerTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dockertraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
