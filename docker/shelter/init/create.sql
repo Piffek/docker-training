4634847d18e95da930f5c2d4da315bcf0219924a
Piwko Patryk <patryk.piwko@god.dev>
2024-04-28 12:57:22 +0200

    use postgress db for shelter

--- docker/shelter/init/create.sql
+++ docker/shelter/init/create.sql
@@ -1,12 +1,9 @@
-DROP TABLE IF EXISTS `place`;
-CREATE TABLE `place`
+DROP TABLE IF EXISTS place;
+CREATE TABLE place
 (
-    `ID`       int unsigned NOT NULL AUTO_INCREMENT,
-    `NAME`     varchar(255) DEFAULT NULL,
-    `CITY`     TEXT         DEFAULT NULL,
-    `POSTCODE` varchar(6)   DEFAULT NULL,
-    `STREET`   TEXT         DEFAULT NULL,
-    PRIMARY KEY (`ID`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+    ID       INT PRIMARY KEY,
+    NAME     varchar(255),
+    CITY     varchar(255),
+    POSTCODE varchar(6),
+    STREET   varchar(255)
+);
