5d8d9d8f3aaaf912555e31802cec4849186ab424
Piwko Patryk <patryk.piwko@god.dev>
2024-05-12 22:16:36 +0200

    add SHELTER_ID

--- pet-db/init/create.sql
+++ pet-db/init/create.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS `pet`;
 CREATE TABLE `pet`
 (
-    `ID`    int unsigned NOT NULL AUTO_INCREMENT,
-    `NAME`  varchar(255) DEFAULT NULL,
-    `FOUND` TEXT         DEFAULT NULL,
+    `ID`         int unsigned NOT NULL AUTO_INCREMENT,
+    `SHELTER_ID` int          NOT NULL,
+    `NAME`       varchar(255) DEFAULT NULL,
+    `FOUND`      TEXT         DEFAULT NULL,
     PRIMARY KEY (`ID`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
